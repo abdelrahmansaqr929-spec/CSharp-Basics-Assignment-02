fb201077b71d9dd769c7d735143911c8bd918be3
abdelrahmansaqr929-spec <abdelrahmansaqr929@gmail.com>
2026-08-27 11:16:22 +0300

    Answer Question 1

---
+++ 01/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01
+{
+    internal class Book 
+    {
+        public string title;
+        public int pages;
+    }
+}
---
+++ 01/Program.cs
@@ -0,0 +1,11 @@
+﻿using _01;
+#region _1 
+object obj = new Book() { title = "quran", pages = 604 };
+Console.WriteLine(obj );
+Console.WriteLine(obj .ToString ());
+Console.WriteLine(obj.Equals(obj));
+Console.WriteLine(obj.GetHashCode());
+Console.WriteLine(obj.GetType());
+#endregion
+
+
